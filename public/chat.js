877922826c28b598b2830bc0336cd13d14ddf9f9
Musiliu Thompson <100306571+the-codeprof@users.noreply.github.com>
2023-06-12 13:48:12 +0100

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:5000')
+const socket = io.connect(process.env.PORT)
 
 const handle = document.querySelector('.handle');
 const message = document.querySelector('.message');
